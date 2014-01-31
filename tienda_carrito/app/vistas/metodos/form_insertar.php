2a447358fa209ed4bebaf1d59ccf066486416c6e
Jesus Maria de Quevedo Tome <jequeto@gmail.com>
2014-01-31 19:41:04 +0100

    borrado de ficheros sobrantes en tienda carrito

--- tienda_carrito/app/vistas/metodos/form_insertar.php
+++
@@ -1,4 +0,0 @@
-<div >
-	<h2>Alta de un método</h2>
-	<?php include "form_and_inputs.php"; ?>
-</div>
--- tienda_carrito/app/vistas/metodos/form_modificar.php
+++
@@ -1,4 +0,0 @@
-<div>
-	<h2>Modificar un método</h2>
-	<?php include "form_and_inputs.php"; ?>
-</div>
