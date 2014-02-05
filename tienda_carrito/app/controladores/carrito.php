0a0077be47608a17fb18c054d2e3f11495c724d0
Jesus Maria de Quevedo Tome <jequeto@gmail.com>
2014-02-05 01:04:20 +0100

    preparando tienda_carrito

--- tienda_carrito/app/controladores/carrito.php
+++ tienda_carrito/app/controladores/carrito.php
@@ -1,40 +1,7 @@
 <?php
 namespace controladores;
 
-class carrito extends \core\Controlador {
+class carrito extends \controladores\carrito_objeto {
 
-	
-	
-	
-	public function anexar_articulo(array $datos=array()) {
-		
-		
-		if ( $validacion = !\core\Validaciones::errores_validacion_request(\modelos\carrito::$validaciones_insert, $datos)) {
-			\modelos\carrito::anexar_articulo($datos, \core\Usuario::$id);
-		}	
-			
-		
-	}
-	
-	
-	
-	
-	
-	public function modificar_articulo(array $datos = array()) {
-		
-	}
-	
-	public function borrar_articulo(array $datos = array()) {
-		
-	}
-	
-	
-	public function contenido(array $datos = array()) {
-		
-	}
-	
-	public function borrar(array $datos = array()) {
-		
-	}
 	
 } // Fin de la clase
---
+++ tienda_carrito/app/controladores/carrito_pedido.php
@@ -0,0 +1,90 @@
+<?php
+namespace controladores;
+
+class carrito_objeto extends \core\Controlador {
+
+	
+	
+	
+	public function meter(array $datos=array()) {
+		
+		if ( $validacion = !\core\Validaciones::errores_validacion_request(\modelos\carrito::$validaciones_insert, $datos)) {
+			
+			$carrito = $this->recuperar();
+			$carrito->meter($datos["values"]);
+			
+		}	
+		
+		header("Location: ".\core\URL::anterior());
+		
+	}
+	
+	
+	
+	
+	
+	public function modificar_articulo(array $datos = array()) {
+		
+	}
+	
+	public function borrar_articulo(array $datos = array()) {
+		
+	}
+	
+	
+	public function ver(array $datos = array()) {
+		
+		$carrito = $this->recuperar();
+		
+		$datos["carrito"] = $carrito;
+		
+		return \core\Vista::generar("carrito_objeto/".__FUNCTION__, $datos);
+//		$http_body = \core\Vista_Plantilla::generar("DEFAULT", $datos);
+//		\core\HTTP_Respuesta::enviar($http_body);
+//		
+		
+	}
+	
+	
+	
+	public function borrar(array $datos = array()) {
+		
+	}
+	
+	
+	/**
+	 * Recupera el carrito guardado en la base de datos.
+	 * Si el usuario logueado tenía un carrito como anónimo y un carrito como usuario logueado
+	 * se recupera el de fecha más reciente y el otro se destruye.
+	 */
+	public function recuperar() {
+		
+		$carrito_anonimo = \modelos\ClaseCarrito::recuperar(session_id());
+		$carrito_usuario = \modelos\ClaseCarrito::recuperar((string)\core\Usuario::$id);
+		$id = (\core\Usuario::$id ? (string)\core\Usuario::$id : session_id());
+		
+		if ( ! $carrito_anonimo and ! $carrito_usuario ) {
+			$carrito = new \modelos\ClaseCarrito($id);
+		}
+		elseif ( $carrito_anonimo and ! $carrito_usuario) {
+			$carrito = $carrito_anonimo;
+			$carrito->cambiar_id($id);
+		}
+		elseif ( ! $carrito_anonimo and $carrito_usuario) {
+			$carrito = $carrito_usuario;
+		}
+		elseif ( $carrito_anonimo and $carrito_usuario) {
+			if ($carrito_anonimo->get_fechaHoraInicio() > $carrito_usuario->get_fechaHoraInicio()) {
+				$carrito = $carrito_anonimo;
+				$carrito->cambiar_id($id);
+			}
+			else {
+				$carrito = $carrito_usuario;
+			}
+		}
+		return $carrito;
+		
+	}
+	
+	
+} // Fin de la clase
