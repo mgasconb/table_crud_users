e9b6b451ec1942b8e2db06e4fa7a0e7f72b5d5dc
Jesus Maria de Quevedo Tome <jequeto@gmail.com>
2014-02-16 11:44:13 +0100

    Optimización de para navegadores Mozilla IE. Solucionado el problema de que un formulario debe contener una tabla entera, no pudiendo estar definido a caballo entre filas de una tabla. Borrado de carritos con antiguedad mayor que 60 minutos.

--- tienda_carrito/app/vistas/menu/index.php
+++ tienda_carrito/app/vistas/menu/index.php
@@ -3,8 +3,7 @@
 	<?php
 		foreach ($datos ["filas"] as $categoria) {
 //			echo "<li class='item'><a href='".\core\URL::generar("expositor/categoria/{$categoria["id"]}")."' title='{$categoria["nombre"]}'>{$categoria["nombre"]}</a></li>\n";
-			echo "<li id='item{$categoria["id"]}' class='item'><a href='".\core\URL::generar("expositor/categoria/{$categoria["nombre"]}/{$categoria["id"]}")."' title='{$categoria["nombre"]}'>{$categoria["nombre"]}</a></li>\n";
-			
+			echo "<li id='item{$categoria["id"]}' class='item'><a href='".\core\URL::generar("expositor/categoria/{$categoria["nombre"]}/{$categoria["id"]}")."' title='{$categoria["nombre"]}'>{$categoria["nombre"]}</a></li>\n";			
 		}
 	?>
 </ul>
